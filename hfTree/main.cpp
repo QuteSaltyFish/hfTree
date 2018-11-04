36f9c6a870782c1b14f3d15a31c13903d10de4f1
QuteSaltyFish <wangmingke@sjtu.edu.cn>
2018-11-04 22:33:54 +0800

    Initial Commit

---
+++ hfTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hfTree
+//
+//  Created by Mingke Wang on 2018/11/4.
+//  Copyright © 2018 Mingke Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
